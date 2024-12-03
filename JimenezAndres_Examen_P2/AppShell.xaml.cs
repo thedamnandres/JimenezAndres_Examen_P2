2c23afb3b2863477f6e771cfc97aa3584f0c1e59
Andres Jimenez <andres_jimenez666@hotmail.com>
2024-12-03 07:22:44 -0500

    Initial commit

---
+++ JimenezAndres_Examen_P2/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace JimenezAndres_Examen_P2;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ JimenezAndres_Examen_P2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace JimenezAndres_Examen_P2;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
