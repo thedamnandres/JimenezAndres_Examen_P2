d329726fa352b6c51ee42cfecc2ec5774bd9087d
Andres Jimenez <andres_jimenez666@hotmail.com>
2024-12-03 07:56:04 -0500

    Gris e iconos

--- JimenezAndres_Examen_P2/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/JimenezAndres_Examen_P2.AssemblyInfo.cs
+++ JimenezAndres_Examen_P2/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/JimenezAndres_Examen_P2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JimenezAndres_Examen_P2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2c23afb3b2863477f6e771cfc97aa3584f0c1e59")]
 [assembly: System.Reflection.AssemblyProductAttribute("JimenezAndres_Examen_P2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JimenezAndres_Examen_P2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
