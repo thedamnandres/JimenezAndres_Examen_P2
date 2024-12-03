5ab2f9b77f78b00e0a4e4b40e591f25372d67fe1
Andres Jimenez <andres_jimenez666@hotmail.com>
2024-12-03 08:08:25 -0500

    Creacion de interfaz y repo de Recargas

--- JimenezAndres_Examen_P2/Interfaces/iRecargas.cs
+++ JimenezAndres_Examen_P2/Interfaces/iRecargas.cs
@@ -1,6 +1,11 @@
+using JimenezAndres_Examen_P2.Models;
+
 namespace JimenezAndres_Examen_P2.Interfaces;
 
-public class iRecargas
+public interface iRecargas
 {
-    
+    Recargas GetRecarga(string numero);
+    bool CreateRecarga(Recargas recarga);
+    bool UpdateRecarga(Recargas recarga);
+    bool DeleteRecarga(string numero);
 }
---
+++ JimenezAndres_Examen_P2/Repositories/RecargasFileRepository.cs
@@ -0,0 +1,54 @@
+using JimenezAndres_Examen_P2.Models;
+using Newtonsoft.Json;
+
+namespace JimenezAndres_Examen_P2.Repositories;
+
+public class RecargasFileRepository : Recargas
+{
+    public string _filePath = Path.Combine(FileSystem.AppDataDirectory, "AndresJimenez.txt");
+    
+    public bool CreateRecarga(Recargas recarga)
+    {
+        try
+        {
+            string json_data = JsonConvert.SerializeObject(recarga);
+            File.WriteAllText(_filePath, json_data);
+            return true;
+        }
+        catch (Exception e)
+        {
+            throw;
+        }
+    }
+    
+    public bool UpdateRecarga(Recargas recarga)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public Recargas GetRecarga(string numero)
+    {
+        Recargas recarga = new Recargas();
+        try
+        {
+            if (File.Exists(_filePath))
+            {
+                string json_data = File.ReadAllText(_filePath);
+                recarga = JsonConvert.DeserializeObject<Recargas>(json_data);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+        return recarga;
+    }
+    
+    public bool DeleteRecarga(string numero)
+    {
+        throw new NotImplementedException();
+    }
+    
+}
